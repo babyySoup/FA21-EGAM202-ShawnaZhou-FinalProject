e78e72ebdc45b0cae16ed0b23c336425452d942f
babyySoup <71286763+babyySoup@users.noreply.github.com>
2021-12-17 18:57:07 -0800

    rq

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject TimerText;
+    public int hoursLeft = 24;
+    public bool takingAway = false;
+
     void Start()
     {
-        
+        TimerText.GetComponent<Text>().text = "hours:" + hoursLeft;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(takingAway == false && hoursLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        //every 15 seconds is 1 hour in game 
+        yield return new WaitForSeconds(15);
+        hoursLeft -= 1;
+        TimerText.GetComponent<Text>().text = "hours:" + hoursLeft;
+        takingAway = false; 
     }
 }
